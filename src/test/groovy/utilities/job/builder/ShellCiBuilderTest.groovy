f6cae723a0ad52a100b49010bede9a230bc161ba
mwolf <maik.wolf@posteo.de>
2016-12-27 10:36:09 +0100

    Move files into groovy dir and replace gmaven plugin with gmaven-plus
    First I readed that is easier to put all groovy classes into the java 
    dir. But with that folder organization, the junit test will not be run.
    So I moved all groovy classes into the groovy dir. 
    
    Also I replaced the gmaven plugin with the newer on gmaven-plus plugin.
    No I can use the newest version of groovy.

---
+++ src/test/groovy/utilities/job/builder/ShellCiBuilderTest.groovy
@@ -0,0 +1,16 @@
+package utilities.job.builder
+
+import org.junit.Test
+
+/**
+ *
+ * @author mwolf
+ */
+class ShellCiBuilderTest extends GroovyTestCase {
+    
+    @Test
+    void testSomething() {
+        assertTrue(true);
+    }
+}
+
